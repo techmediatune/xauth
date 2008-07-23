639e7741c6b0946982cc86ba44f86bcccd9fc868
neildogg <neildogg@2bbbc9bc-a452-0410-97f3-01d036f82573>
2008-07-23 03:26:27 +0000

    API improvements, more robust test

--- js/xauth/xauth.js
+++ js/xauth/xauth.js
@@ -6,37 +6,59 @@ xauth = {
 		//		iframe somewhere within its contents
 		// kwArgs.url:
 		//		The URL of the third-party site
-		var node = kwArgs.node, url = kwArgs.url;
+		var node = kwArgs.node;
+		var url = kwArgs.url;
+
 		if(!node || !node.nodeType == 1 || !url){
 			throw new Error("Pass xauth.init an object containing the attributes `node` and `url`");
 		}
-		xauth.url = url;
-		xauth.node = node;
-		var iframe = (node.tagName == "IFRAME") ? node : document.getElementsByTagName("iframe", node)[0];
+
+		var iframe = kwArgs.iframe = (node.tagName == "IFRAME") ? node : document.getElementsByTagName("iframe", node)[0];
 		if(!iframe){
-			throw new Error("Passed node doesn't contain an iframe element");
+			throw new Error("Pass xauth.init a node that contains an iframe element");
 		}
-		xauth.iframe = iframe;
-		if(!xauth.loaded){
-			// Handle page reloads
-			iframe.src = iframe.src;
+
+		// Make sure to deal with problems due to reloading the page
+		iframe.src = iframe.src;
+
+		xauth._stack.push(kwArgs);
+
+		kwArgs._callbacks = [];
+		kwArgs.addCallback = xauth._addCallback;
+
+		return kwArgs;
+	},
+	_addCallback: function(/*Object?*/ context, /*Function*/ callback){
+		if(arguments.length > 1){
+			this._callbacks.push([context, callback])
 		}else{
-			xauth._init();
+			this._callbacks.push([{}, context])
 		}
 	},
-	_callbacks: [],
-	addOnLoad: function(/*Function*/ callback){
-		xauth._callbacks.push(callback);
-	},
-	_init: function(callback){
-		xauth.loaded = true;
-		if(xauth.url){
-			document.getElementsByTagName("iframe")[0].contentWindow.location = xauth.url;
+	_stack: [],
+	_getStack: function(child){
+		for(var i=0, s; s=xauth._stack[i]; i++){
+			if(s.iframe.contentWindow == child){
+				return s;
+			}
 		}
 	},
-	_callback: function(status, token){
-		for(var c=xauth._callbacks, i=0, l=c.length; i<l; i++){
-			c[i](status, token, xauth.node);
+	_init: function(child){
+		var stack = xauth._getStack(child);
+		if(stack){
+			stack.iframe.contentWindow.location = stack.url;
+		}
+	},
+	_callback: function(child){
+		var stack = xauth._getStack(child);
+		if(stack){
+			if(!child.name && !child.name.length){
+				throw new Error("xauth expects server to assign a value to window.name");
+			}
+
+			for(var i=0, c; c=stack._callbacks[i]; i++){
+				c[1].call(c[0], parseInt(child.name.charAt(0)), child.name.slice(1), stack.node);
+			}
 		}
 	}
 }
