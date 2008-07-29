f0dd1deca9b345c442e290b2658200950b20ca1f
neildogg <neildogg@2bbbc9bc-a452-0410-97f3-01d036f82573>
2008-07-29 00:31:46 +0000

    Properly detect referrer

--- tests/test_cookie_approval.php
+++ tests/test_cookie_approval.php
@@ -2,6 +2,7 @@
 session_start();
 $referrer = parse_url($_SERVER['HTTP_REFERER']);
 if ($referrer['host'] != $_SERVER['HTTP_HOST']) $_SESSION['referrer'] = $referrer['host'];
+elseif ($_POST['referrer']) $_SESSION['referrer'] = $_POST['referrer'];
 ?>
 <?php if ($_COOKIE['test_cookie_approval'] == 'yes' && $_SESSION['authorized'] && $_SESSION['authorized'][$_SESSION['referrer']]): ?>
 <html>
@@ -47,6 +48,7 @@ window.location = redirect + "#xauth=0";
 	<body>
 		<p>Log in <a href="test_cookie_approval_popup.php" target="_new">here</a> and then click "Continue".</p>
 		<form method="POST" action="test_cookie_approval.php">
+			<input type="hidden" name="referrer" value="<?php print $_SESSION['referrer']; ?>" />
 			<input type="submit" name="continue" value="Continue" />
 		</form>
 	</body>
